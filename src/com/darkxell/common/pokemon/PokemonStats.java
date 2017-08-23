1c987e01fd26d75d41dd09e3d230e33d45753c1a
Cubiccl <ccl.clement@hotmail.fr>
2017-08-23 13:57:19 +0200

    Species class has all necessary data

--- src/com/darkxell/common/pokemon/PokemonStats.java
+++ src/com/darkxell/common/pokemon/PokemonStats.java
@@ -3,16 +3,21 @@ package com.darkxell.common.pokemon;
 public class PokemonStats
 {
 
+	/** Attack. */
 	public final int attack;
+	/** Defense. */
 	public final int defense;
+	/** Health Points. */
 	public final int health;
+	/** Special Attack. */
 	public final int specialAttack;
+	/** Special Defense. */
 	public final int specialDefense;
+	/** Speed. */
 	public final int speed;
 
 	public PokemonStats(int attack, int defense, int health, int specialAttack, int specialDefense, int speed)
 	{
-		super();
 		this.attack = attack;
 		this.defense = defense;
 		this.health = health;
