d6e7b4c44cb5f5410c9c3449bba5edfbee61cb75
Cubiccl <ccl.clement@hotmail.fr>
2017-08-23 13:38:47 +0200

    Move class has all necessary data

--- src/com/darkxell/common/move/Move.java
+++ src/com/darkxell/common/move/Move.java
@@ -4,6 +4,16 @@ import com.darkxell.common.pokemon.PokemonType;
 
 public abstract class Move
 {
+	/** Move targets.<br />
+	 * <ul>
+	 * <li>ONE = 0</li>
+	 * <li>ONE_AUTO = 1</li>
+	 * <li>SELF = 2</li>
+	 * <li>ALL_ENEMIES = 3</li>
+	 * <li>ALL_ALLIES = 4</li>
+	 * <li>ALL_ADJACENT = 5</li>
+	 * </ul> */
+	public static final byte ONE = 0, ONE_AUTO = 1, SELF = 2, ALL_ENEMIES = 3, ALL_ALLIES = 4, ALL_ADJACENT = 5;
 	/** Move categories.<br />
 	 * <ul>
 	 * <li>PHYSICAL = 0</li>
@@ -14,28 +24,44 @@ public abstract class Move
 
 	/** This move's accuracy. */
 	public final int accuracy;
+	/** If this move has an additional effect, its chance to happen. */
+	public final int additionalEffectChance;
 	/** This move's category. See {@link Move#PHYSICAL}. */
 	public final byte category;
 	/** This move's ID. */
 	public final int id;
+	/** True if this move makes contact. */
+	public final boolean makesContact;
 	/** This move's name. */
 	public final String name;
 	/** This move's power. */
 	public final int power;
-	/** This move's Power Points. */
+	/** This move's default Power Points. */
 	public final int pp;
+	/** This move's maximum Power Points. */
+	public final int ppMax;
+	/** This move's priority. */
+	public final int priority;
+	/** This move's targets. See {@link Move#SINGLE}. */
+	public final byte targets;
 	/** This move's type. */
 	public final PokemonType type;
 
-	public Move(int id, String name, PokemonType type, byte category, int power, int pp, int accuracy)
+	public Move(int id, String name, PokemonType type, byte category, int pp, int ppMax, int power, int accuracy, byte targets, int priority,
+			int additionalEffectChance, boolean makesContact)
 	{
 		this.id = id;
 		this.name = name;
 		this.type = type;
 		this.category = category;
-		this.power = power;
 		this.pp = pp;
+		this.ppMax = ppMax;
+		this.power = power;
 		this.accuracy = accuracy;
+		this.targets = targets;
+		this.priority = priority;
+		this.additionalEffectChance = additionalEffectChance;
+		this.makesContact = makesContact;
 	}
 
 }
